28169e52ee58815462add4fbbd8d67cc0ad6bea9
nelcambo <nelcambo@gmail.com>
2023-03-31 22:44:55 -0500

    Rescueing My Files

---
+++ Pyhton Institute/Python Esentials 1/Module 2/2.6 Inputting Data/2.6.1.15 PE1 -- Module 2 Test.py
@@ -0,0 +1,26 @@
+# x = 1 / 2 + 3 // 3 + 4 **2
+# print (x)
+
+# x = int(input())
+# y = int(input()) 
+
+# x = x % y 
+# x = x % y 
+# y = y % x 
+
+#input 11 and 4 
+#My guess is 1, lets see
+
+# print(y)
+# print()
+
+#print(1 // 2 * 3)
+
+# x = int(input())
+# y = int(input()) 
+
+# x = x // y 
+# y = y // x 
+
+# print(y)
+
---
+++ Youtube Course/datatype.py
@@ -0,0 +1,31 @@
+from asyncore import read
+
+
+print("hello world")
+print('Hello World')
+print('''HELLO world''')
+print(""""Hello World""")
+
+print(type('chutica'))
+
+# List A i s a list and it can change
+
+A=[10,20,30,55]
+print(A)
+print(type(A))
+print('A mi me pica' + ' la chutica')
+
+# Tuples B is a tuple and it cannot change
+
+B=(10,20,30,55)
+print(B)
+print(type(B))
+
+# Dictionaries
+# C es un diccionario
+C={'nombre': 'Ryan', 'apellido': 'Fonseca', 'alias': 'Ry', 'edad': 25, 'lunac': 'Barquisimeto'}
+print(C)
+print(type(C))
+
+# None es tipo de dato sin nada
+None 
